932eaa7949a7013034b5dbb75a450ebada34a6ce
Josh <JoshLynch2001@gmail.com>
2025-04-09 19:16:34 -0300

    this might take some planning

--- src/renderer/src/components/ordersPage/orderReview/orderReview.jsx
+++ src/renderer/src/components/ordersPage/orderReview/orderReview.jsx
@@ -4,11 +4,16 @@ import { Navbar } from "../../General/navbar/navbar";
 export const OrderReview = () => {
 
     return (
-        
         <div className="order-review-page">
             <Navbar/>
             <OrdersNavbar/>
             <h1 className='main-title-text'>Order Review</h1>
+            <div className="review-button-container">
+                <button className="review-button">Create Orders</button>                
+            </div>
+            <div className="card-container">
+                <div className="bubble"></div>
+            </div>
         </div>
     );
 }
